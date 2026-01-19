97fb76ae1af37537350b6c7741ef749a46b02d88
Swaroop883 <saiswaroopguntukusaiswaroop@gmail.com>
2026-01-19 13:55:09 +0530

    added comments

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "postgresql://postgres:Swaroop%402006@localhost:5432/auth_db"
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)#converts the python commands into raw sql query which are understood by the sql database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+Base = declarative_base()#retruns a class
 def get_db():
     db = SessionLocal()
     try:
-        yield db
+        yield db #allows the funcction to stay alive while your database work is happening
     finally:
         db.close()
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
+#here the user class is inheriting from the base class , and gets access to the metadata
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
